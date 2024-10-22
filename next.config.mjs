f123ab5150ccb864f135ad34899a984ab16bbd28
Pierre-Louis Létoquart <randlgint@proton.me>
2024-10-21 20:47:02 -0400

    chore: custom cache handler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  cacheHandler: path.resolve('./cache-handler.mjs'),
+  // Nginx will do gzip compression. We disable
+  // compression here so we can prevent buffering
+  // streaming responses
+  compress: false,
 };
 
 export default nextConfig;
